171638a81bcc91fb9ef373fc63c65fd9a8583530
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-06-10 20:28:25 -0700

    give encounter credit on in-world capture

--- src/main/kotlin/us/timinc/mc/cobblemon/counter/event/PokemonCapture.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/counter/event/PokemonCapture.kt
@@ -2,10 +2,12 @@ package us.timinc.mc.cobblemon.counter.event
 
 import com.cobblemon.mod.common.api.events.pokemon.PokemonCapturedEvent
 import us.timinc.mc.cobblemon.counter.api.CaptureApi
+import us.timinc.mc.cobblemon.counter.api.EncounterApi
 
 object PokemonCapture {
     fun handle(event: PokemonCapturedEvent) {
         val species = event.pokemon.species.name.lowercase()
+        EncounterApi.add(event.player, species)
         CaptureApi.add(event.player, species)
     }
 }
